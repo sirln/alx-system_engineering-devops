45ca89f7e8a6d586e54de20aed60742b2cd2dab3
sirln <siro@sirlawren.com>
2023-10-05 22:24:36 +0300

    Load Balancer, uploading puppet script for custom HTTP header response

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -12,10 +12,13 @@ service { 'nginx':
   require => Package['nginx'],
 }
 
+# Capture the hostname using Puppet's facter
+$server_hostname = $facts['networking']['hostname']
+
 # Add the custom HTTP header to the default site configuration
 file_line { 'nginx_custom_header':
   path    => '/etc/nginx/sites-available/default',
-  line    => 'add_header X-Served-By $hostname;',
+  line    => "add_header X-Served-By ${server_hostname};",
   match   => '^add_header X-Served-By',
   require => Package['nginx'],
   notify  => Service['nginx'],  # Restart Nginx if the file changes
