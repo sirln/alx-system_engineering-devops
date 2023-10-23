89c7562106fdec02988af675c5314b112e16c413
sirln <siro@sirlawren.com>
2023-10-24 01:17:03 +0300

    API, Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''
+Python REST API
+'''
+import csv
+import json
+import requests
+from sys import argv
+
+
+def get_all_empolyee_tasks():
+    # Define URLs to fetch data from
+    api_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{api_url}/users"
+    todos_url = f"{api_url}/todos"
+
+    # Fetch user information
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+
+    # Fetch TODOs for the user
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Organize data
+    organized_data = {}
+
+    for user in user_data:
+        user_id = user["id"]
+        username = user["name"]
+        organized_data[user_id] = []
+
+        for todo in todos_data:
+            task_info = {
+                "username": username,
+                "task": todo['title'],
+                "completed": todo['completed']
+            }
+            organized_data[user_id].append(task_info)
+
+    with open(f"todo_all_employees.json", "w") as file:
+        json.dump(organized_data, file, indent=4)
+
+    print("Data exported to todo_all_employees.json!")
+
+
+if __name__ == '__main__':
+    get_all_empolyee_tasks()
