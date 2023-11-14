d80697394b8231f0a7b5201817fa2ebe86a930df
sirln <siro@sirlawren.com>
2023-11-14 15:14:05 +0300

    Web stack debugging #4, puppet script file to increase nginx ulimit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,16 @@
+# ulimit configuration script to increase the amount of requests a nginx server can handle
+file {'/etc/default/nginx':
+    ensure  => file,
+}
+
+file_line {'configure nginx ulimit':
+    path    => '/etc/default/nginx',
+    line    => 'ULIMIT="-n unlimited"',
+    match   => '^ULIMIT="-n 15"$',
+}
+
+service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/default/nginx'],
+}
