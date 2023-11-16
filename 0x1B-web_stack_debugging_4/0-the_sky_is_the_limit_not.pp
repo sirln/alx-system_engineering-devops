c56c6a0b402b59e0fb006090b0d928c3c2227e1b
sirln <siro@sirlawren.com>
2023-11-16 19:18:22 +0300

    Web stack debugging #4, Sky is the limit, let's bring that limit higher correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,10 @@
 # ulimit configuration script to increase the amount of requests a nginx server can handle
-file {'/etc/default/nginx':
-    ensure  => file,
+exec { 'adjust_nginx_ulimit':
+  command => 'sed -i \'s/^ULIMIT="-n 15"$/ULIMIT="-n unlimited"/\' /etc/default/nginx',
+  path    => '/usr/bin:/bin',
 }
 
-exec { 'adjust_nginx_ulimit':
-  command     => 'sed -i \'s/^ULIMIT="-n 15"$/ULIMIT="-n unlimited"/\' /etc/default/nginx',
-  path        => '/usr/bin:/bin',
-  notify      => Service['nginx'],
-  refreshonly => true,
+exec { 'restart_nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
